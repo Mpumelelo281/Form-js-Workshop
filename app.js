26351a8966e43a70050a44cb47a67e5324366174
mpumelelo281 <ngobesempumelelo2@gmail.com>
2025-08-19 17:07:46 +0200

    Updated app.js, index.html, app.css and formControl

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
-import NumberWork from "./numberWork.js";
 
-const num1 = new NumberWork(24);
-num1.displayNumber();
-num1.pozOrNeg();
+import formControl1 from "./formControl.js";
+
+function getUserName() {
+  const username = localStorage.getItem("userName");
+  const userNameElement = document.getElementById("username");
+  const getNameSection = document.getElementById("getName");
+  const choiceSection = document.getElementById("greeting");
+
+  if (username) {
+    getNameSection.style.display = "none";
+    choiceSection.style.display = "block";
+    userNameElement.textContent = username;
+  }
+}
+
+getUserName();
+
+window.setName = () => {
+  const nameInput = document.getElementById("usernameField");
+  localStorage.setItem("userName", nameInput.value);
+  const getNameSection = document.getElementById("getName");
+  const choiceSection = document.getElementById("greeting");
+
+  getNameSection.style.display = "none";
+  choiceSection.style.display = "block";
+};
+
+// const num1 = new NumberWork(24);
+// num1.displayNumber();
+// num1.pozOrNeg();
+
+const numberInput = document.getElementById("calculate");
+localStorage.setItem("lastName", "Ngobese");
+let form1;
+
+window.start = (event) => {
+  event.preventDefault();
+  const num = numberInput.value;
+  const email = document.getElementById("email").value;
+  form1 = new formControl1(num, email);
+  form1.submithandler();
+};
+
+window.onYes = () => {
+  showForm();
+};
+
+window.onNo = () => {
+  hideForm();
+};
+
+const showForm = () => {
+  document.getElementById("form").style.display = "block";
+  document.getElementById("greeting").style.display = "none";
+};
+const hideForm = () => {
+  document.getElementById("greeting").style.display = "none";
+  document.getElementById("feedback").style.display = "block";
+};
--- formControl.js
+++ formControl.js
@@ -1,9 +1,41 @@
-import NumberWork from "./numberWork.js"
 
-// class formControl1{
-//     constructor2 (num1)
-//     this.num1 = num1
-//     submithandler(){
-//         success: x
-//     }
-// }
+
+export default class formControl1 {
+  constructor(num1, email) {
+    this.num1 = num1;
+    this.email = email;
+    let isValid = document.getElementById("email");
+    // this.getName();
+  }
+
+  submithandler() {
+    let errorMessage = document.getElementById("error-message");
+    errorMessage.style.visibility = "hidden";
+    if (this.checkIsNotNumber(this.num1)) {
+      errorMessage.style.visibility = "visible";
+    } else {
+      alert("The number is " + this.num1);
+    }
+    if (!this.isValidEmail(this.email)) {
+      alert("The email is not valid!");
+    } else {
+      alert("Email is valid.");
+    }
+  }
+
+//   getName() {
+//     console.log(localStorage.getItem("lastname"));
+//   }
+
+  checkIsNotNumber(number) {
+    return isNaN(number) || !number;
+  }
+
+  isValidEmail(email) {
+    const regex = /^[^s@]+@[^s@]+.[^s@]{2,}$/;
+    console.log(email);
+    return regex.test(email);
+  }
+
+  
+}
